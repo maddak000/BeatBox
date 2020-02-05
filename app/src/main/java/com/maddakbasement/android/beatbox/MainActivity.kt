0a98ff7cb644addf1808d4d2f6e2c2a1c3d8447a
Marcin Dworak <marcin.dworak@gmail.com>
2020-02-05 21:09:37 +0100

    Setup data binding for MainActivity

--- app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
@@ -2,11 +2,19 @@ package com.maddakbasement.android.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.maddakbasement.android.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
     }
 }
