3f73efca035d9c3f93ca7afdf459fcf875438c7a
Marcin Dworak <marcin.dworak@gmail.com>
2020-02-09 20:46:50 +0100

    Created assets folder and added sounds to sample_sounds sub-folder

--- app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.maddakbasement.android.beatbox
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
@@ -29,14 +30,9 @@ class MainActivity : AppCompatActivity() {
     private inner class SoundAdapter() :
         RecyclerView.Adapter<SoundHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-                SoundHolder {
-            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
-                layoutInflater,
-                R.layout.list_item_sound,
-                parent,
-                false
-            )
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding> (
+                layoutInflater, R.layout.list_item_sound,parent,false)
             return SoundHolder(binding)
         }
 
