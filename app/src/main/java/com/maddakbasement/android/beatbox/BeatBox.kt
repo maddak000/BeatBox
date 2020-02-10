d6813952cd0e7fba290a3b06d598b8a850d2f63a
Marcin Dworak <marcin.dworak@gmail.com>
2020-02-10 21:49:32 +0100

    Implemented loadSounds in BeatBox

--- app/src/main/java/com/maddakbasement/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/BeatBox.kt
@@ -1,7 +1,27 @@
 package com.maddakbasement.android.beatbox
 
+import android.content.res.AssetManager
+import android.util.Log
+
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
 
-class BeatBox {
+class BeatBox(private val assets: AssetManager) {
+
+    fun loadSounds(): List<String> {
+        val soundNames = assets.list(SOUNDS_FOLDER)
+        Log.d(TAG, "Found ${soundNames?.size ?: 0} sounds")
+        return soundNames?.asList() ?: emptyList()
+    }
+
+//    fun loadSounds(): List<String> {
+//        return try {
+//            val soundNames = assets.list(SOUNDS_FOLDER)!!
+//            Log.d(TAG, "Found ${soundNames.size} sounds")
+//            soundNames.asList()
+//        } catch (e: Exception) {
+//            Log.e(TAG, "Could not list assets", e)
+//            emptyList()
+//        }
+//    }
 }
