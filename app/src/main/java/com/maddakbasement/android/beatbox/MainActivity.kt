cd3cc53f9522613128168ea7a7efd089afd29125
Marcin Dworak <marcin.dworak@gmail.com>
2020-02-11 21:39:52 +0100

    Implemented onBindViewHolder to finalize hooking up the Adapter

--- app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/MainActivity.kt
@@ -38,6 +38,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            val sound = sounds[position]
+            holder.bind(sound)
         }
 
         override fun getItemCount() = sounds.size
