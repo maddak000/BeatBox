236273e5465423da04364d2e4ad6534f6408aef2
Marcin Dworak <marcin.dworak@gmail.com>
2020-02-13 21:21:13 +0100

    Created load function on BeatBox to load all sounds to SoundPool

--- app/src/main/java/com/maddakbasement/android/beatbox/BeatBox.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/BeatBox.kt
@@ -1,8 +1,10 @@
 package com.maddakbasement.android.beatbox
 
+import android.content.res.AssetFileDescriptor
 import android.content.res.AssetManager
 import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
@@ -25,9 +27,20 @@ class BeatBox(private val assets: AssetManager) {
         soundNames.forEach { filename ->
             val assetPath = "$SOUNDS_FOLDER/$filename"
             val sound = Sound(assetPath)
-            sounds.add(sound)
+            try {
+                load(sound)
+                sounds.add(sound)
+            } catch (ioe: IOException){
+                Log.e(TAG, "Could not load sound $filename", ioe)
+            }
         }
         return sounds
+    }
+
+    private fun load(sound: Sound){
+        val afd: AssetFileDescriptor = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd,1)
+        sound.soundID = soundId
     }
 
 //    fun loadSounds(): List<String> {
--- app/src/main/java/com/maddakbasement/android/beatbox/Sound.kt
+++ app/src/main/java/com/maddakbasement/android/beatbox/Sound.kt
@@ -2,6 +2,6 @@ package com.maddakbasement.android.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundID: Int? = null) {
     val name= assetPath.split("/").last().removeSuffix(WAV)
 }
